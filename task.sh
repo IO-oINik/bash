c1a1880e91c0ee90181b2c4b3e7b0e0e006d90df
IO-oINik <nik.mokshin.2004@mail.ru>
2024-12-22 00:36:20 +0300

    added description task

--- task.sh
+++ task.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#7.Сохранить в файл выдержку из мануала к команде ls (man ls), в которой будет описано использование ключа -r.
+
 output_file="result.txt"
 
 result=$(ls --help | awk '/-r, --reverse/{flag=1; print; next} flag && /^[[:space:]]*[^-]/{flag=0} flag')
